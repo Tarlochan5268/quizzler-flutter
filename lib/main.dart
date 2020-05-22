d1d75b4adfec51941c40b71e07177c19481b2397
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-22 03:31:12 -0400

    Removed replaced widget list in row with custom dynamic widgets of icon which gets added and displayed on screen only when user click on true or false button

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -61,6 +63,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(
+                  () {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                  },
+                );
                 //The user picked true.
               },
             ),
@@ -79,22 +91,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(
+                  () {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  },
+                );
                 //The user picked false.
               },
             ),
           ),
         ),
         Row(
-          children: <Widget>[
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-          ],
+          children: scoreKeeper,
         ),
         //TODO: Add a Row here as your score keeper
       ],
