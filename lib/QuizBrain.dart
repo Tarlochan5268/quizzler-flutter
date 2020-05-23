a946bd790869633eedbfb5404a6e99c76304b318
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-23 06:39:01 -0400

    Added Quiz brain class to store all questions list

---
+++ lib/QuizBrain.dart
@@ -0,0 +1,36 @@
+import 'package:quizzler/Question.dart';
+
+class QuizBrain {
+  List<Question> questionsBank = [
+    new Question('You can lead a cow down stairs but not up stairs.', false),
+    new Question(
+        'Approximately one quarter of human bones are in the feet.', true),
+    new Question('A slug\'s blood is green.', true),
+    new Question('Some cats are actually allergic to humans', true),
+    new Question('You can lead a cow down stairs but not up stairs.', false),
+    new Question(
+        'Approximately one quarter of human bones are in the feet.', true),
+    new Question('A slug\'s blood is green.', true),
+    new Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    new Question('It is illegal to pee in the Ocean in Portugal.', true),
+    new Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    new Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    new Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    new Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    new Question('Google was originally called \"Backrub\".', true),
+    new Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    new Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/Question.dart';
+import 'package:quizzler/QuizBrain.dart';
 
+QuizBrain quizBrain = new QuizBrain();
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -29,12 +30,6 @@ class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
   int questionNumber = 0;
 
-  List<Question> questionsList = [
-    new Question(q:'You can lead a cow down stairs but not up stairs.',a:false),
-    new Question(q:'Approximately one quarter of human bones are in the feet.',a:true),
-    new Question(q:'A slug\'s blood is green.',a:true)
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -47,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionsList[questionNumber].question,
+                quizBrain.questionsBank[questionNumber].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
