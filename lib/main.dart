503d1566bcb1b97a4193e50b9d4fd2e36dad15e9
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-23 06:28:25 -0400

    Added Question class to add question and answer to it into one structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/Question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,12 +27,13 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  int questionNumber = 0;
+
+  List<Question> questionsList = [
+    new Question(q:'You can lead a cow down stairs but not up stairs.',a:false),
+    new Question(q:'Approximately one quarter of human bones are in the feet.',a:true),
+    new Question(q:'A slug\'s blood is green.',a:true)
   ];
-  List<bool> answers = [false, true, true];
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionsList[questionNumber].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
